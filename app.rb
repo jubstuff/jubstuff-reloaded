f3576188b2e18f0685f79d7dc372dc3a8f9af82c
Giustino Borzacchiello <giustinob@gmail.com>
2011-09-07 23:18:19 +0200

    Prima bozza blog

---
+++ app.rb
@@ -0,0 +1,17 @@
+module Nesta
+	class App
+		helpers do
+			# mostra gli ultimi post
+			def ultimi_post(articles)
+				haml(:summaries, :layout => false, :locals => { :pages => articles })
+			end
+		end
+	end
+
+	class Page
+		# Testo mostrato di default al sommario dei post
+		def read_more
+			'Read more'
+		end
+	end
+end
